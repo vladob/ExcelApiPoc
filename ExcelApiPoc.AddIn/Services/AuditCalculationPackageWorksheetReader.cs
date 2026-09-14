e30951418424a52b6743227f369b37f5f492e2bd
Vlado Bosnjakovic <mobil.fellow@gmail.com>
2026-09-14 14:18:31 +0200

    Keep embedded package reader backward compatible

--- ExcelApiPoc.AddIn/Services/AuditCalculationPackageWorksheetReader.cs
+++ ExcelApiPoc.AddIn/Services/AuditCalculationPackageWorksheetReader.cs
@@ -25,8 +25,17 @@ namespace ExcelApiPoc.AddIn.Services
                     MfSpecification = AuditWorkbookTableReader.GetString(metadata, "MfSpecification"),
                     ValidFrom = AuditWorkbookTableReader.GetNullableDateTime(metadata, "ValidFrom"),
                     ValidTo = AuditWorkbookTableReader.GetNullableDateTime(metadata, "ValidTo"),
-                    CreateMultiYear = AuditWorkbookTableReader.GetBoolean(metadata, "CreateMultiYear"),
-                    MultiYearWorksheetName = AuditWorkbookTableReader.GetString(metadata, "MultiYearWorksheetName"),
+                    CreateMultiYear =
+                        metadata.ContainsKey("CreateMultiYear") &&
+                        AuditWorkbookTableReader.GetBoolean(
+                            metadata,
+                            "CreateMultiYear"),
+                    MultiYearWorksheetName =
+                        metadata.ContainsKey("MultiYearWorksheetName")
+                            ? AuditWorkbookTableReader.GetString(
+                                metadata,
+                                "MultiYearWorksheetName")
+                            : null,
                     AccountingModel = AuditWorkbookTableReader.GetString(metadata, "AccountingModel"),
                     Tables = tables
                 },
