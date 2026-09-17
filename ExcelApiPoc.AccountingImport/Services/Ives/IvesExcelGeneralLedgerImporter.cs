44eb080ce4e87bbb859cd107e8cdd04a74aff308
Vlado Bosnjakovic <mobil.fellow@gmail.com>
2026-09-17 12:19:03 +0200

    Resolve IVES ledger metadata from content and filename

--- ExcelApiPoc.AccountingImport/Services/Ives/IvesExcelGeneralLedgerImporter.cs
+++ ExcelApiPoc.AccountingImport/Services/Ives/IvesExcelGeneralLedgerImporter.cs
@@ -38,6 +38,28 @@ namespace ExcelApiPoc.AccountingImport.Services.Ives
                 throw CreateValidationException(report);
             }
 
+            AccountingFileNameMetadata fileNameMetadata = null;
+            if (AccountingFileNameMetadataParser.TryParse(
+                    source.SourceFileName,
+                    out AccountingFileNameMetadata parsedMetadata))
+            {
+                if (parsedMetadata.DocumentKind !=
+                    AccountingSourceDocumentKind.GeneralLedger)
+                {
+                    throw new InvalidDataException(
+                        "Filename '" + source.SourceFileName +
+                        "' does not identify a general ledger.");
+                }
+
+                fileNameMetadata = parsedMetadata;
+            }
+
+            int fiscalYear =
+                AccountingFileNameMetadataParser.ResolveFiscalYear(
+                    source.SourceFileName,
+                    source.FiscalYear,
+                    fileNameMetadata);
+
             var result = new GeneralLedgerImport
             {
                 SourceFileName = source.SourceFileName,
@@ -45,14 +67,18 @@ namespace ExcelApiPoc.AccountingImport.Services.Ives
                 SourceFileHash = CalculateSha256(source.SourceFilePath),
                 TechnicalType = "Excel",
                 AccountingFormat = "IVES",
-                Ico = source.Ico,
-                FiscalYear = source.FiscalYear,
+                Ico = AccountingFileNameMetadataParser.ResolveIco(
+                    source.SourceFileName,
+                    source.Ico,
+                    fileNameMetadata),
+                FiscalYear = fiscalYear,
+                ExportStage = fileNameMetadata?.ExportStage,
                 ThroughMonth = source.PeriodEnd.Value.Month,
                 PeriodHeader =
                     source.PeriodEnd.Value.Month.ToString(
                         CultureInfo.InvariantCulture) +
                     "/" +
-                    source.FiscalYear.ToString(
+                    fiscalYear.ToString(
                         CultureInfo.InvariantCulture),
                 ImportedAtUtc = DateTime.UtcNow,
                 ImportReport = report
