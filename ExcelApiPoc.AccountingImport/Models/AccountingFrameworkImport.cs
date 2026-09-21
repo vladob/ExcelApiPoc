e7edead1bc520f55435451c58bf92a05324adb59
Vlado Bosnjakovic <mobil.fellow@gmail.com>
2026-09-21 14:14:02 +0200

    Move accounting-framework model into import layer

--- ExcelApiPoc.AccountingImport/Models/AccountingFrameworkImport.cs
+++ ExcelApiPoc.AccountingImport/Models/AccountingFrameworkImport.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace ExcelApiPoc.AddIn.Models
+namespace ExcelApiPoc.AccountingImport.Models
 {
-    internal enum AccountingFrameworkRowKind
+    public enum AccountingFrameworkRowKind
     {
         Empty,
         GroupHeading,
@@ -11,7 +11,7 @@ namespace ExcelApiPoc.AddIn.Models
         AnalyticalAccount
     }
 
-    internal sealed class AccountingFrameworkImport
+    public sealed class AccountingFrameworkImport
     {
         public string SourceFileName { get; set; }
         public string SourceFilePath { get; set; }
@@ -24,25 +24,39 @@ namespace ExcelApiPoc.AddIn.Models
         public int? ExportStage { get; set; }
         public DateTime ImportedAtUtc { get; set; }
         public int NormalizedTextFieldCount { get; set; }
+
         public List<AccountingFrameworkRow> Rows { get; } =
             new List<AccountingFrameworkRow>();
     }
 
-    internal sealed class AccountingFrameworkRow
+    public sealed class AccountingFrameworkRow
     {
         public int SequenceNumber { get; set; }
         public int SourceRecordNumber { get; set; }
+
         public string SourceSyntheticCode { get; set; }
         public string SourceAnalyticalCode { get; set; }
+
         public string SyntheticCode { get; set; }
         public string AnalyticalCode { get; set; }
         public string AccountCode { get; set; }
         public string AccountName { get; set; }
         public string Type { get; set; }
+
         public string SubsidiaryFlag { get; set; }
         public string TaxFlag { get; set; }
         public string BalanceFlag { get; set; }
         public string VatFlag { get; set; }
+
+        public string ActivityCode { get; set; }
+        public string PsFlag { get; set; }
+        public string BuFlag { get; set; }
+        public string RuFlag { get; set; }
+        public string PlFlag { get; set; }
+        public string Currency { get; set; }
+        public DateTime? ValidFrom { get; set; }
+        public DateTime? ValidTo { get; set; }
+
         public AccountingFrameworkRowKind RowKind { get; set; }
     }
 }
