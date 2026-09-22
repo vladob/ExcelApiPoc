beffa446ef9f9fe0cc4a4c0fcc2e1a8e5c572921
Vlado Bosnjakovic <mobil.fellow@gmail.com>
2026-09-22 14:53:24 +0200

    Fix IfoSoft ledger recovery build

--- ExcelApiPoc.AccountingImport/Services/IfoSoft/IfoSoftCsvGeneralLedgerImporter.cs
+++ ExcelApiPoc.AccountingImport/Services/IfoSoft/IfoSoftCsvGeneralLedgerImporter.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
